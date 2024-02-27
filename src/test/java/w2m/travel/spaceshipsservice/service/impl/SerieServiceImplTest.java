96733558c5f6c487f666a6d7cb59166f3ee9dea3
damiansarracent <damian.sarracent@in2.es>
2024-02-27 18:37:54 -0300

    Implementing change log with liquibase

---
+++ src/test/java/w2m/travel/spaceshipsservice/service/impl/SerieServiceImplTest.java
@@ -0,0 +1,15 @@
+package w2m.travel.spaceshipsservice.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SerieServiceImplTest {
+
+    @Test
+    void createSerie() {
+      }
+
+    @Test
+    void getAllSeries() {
+      }
+}
---
+++ src/test/java/w2m/travel/spaceshipsservice/service/impl/SpaceshipServiceImplTest.java
@@ -0,0 +1,35 @@
+package w2m.travel.spaceshipsservice.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SpaceshipServiceImplTest {
+
+    @Test
+    void getAllSpaceships() {
+      }
+
+    @Test
+    void getSpaceshipById() {
+      }
+
+    @Test
+    void getSpaceshipsByNameContaining() {
+      }
+
+    @Test
+    void createSpaceship() {
+      }
+
+    @Test
+    void deleteSpaceship() {
+      }
+
+    @Test
+    void updateSpaceship() {
+      }
+
+    @Test
+    void createSerieForSpaceship() {
+      }
+}
